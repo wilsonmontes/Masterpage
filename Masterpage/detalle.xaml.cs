25273e382369c4d27df4c413658a3a701140722a
wilson <Aprendiz Sena@TUNDFPCGE7F363.FormacionSena.RED>
2018-05-02 20:34:48 -0500

    Initial commit

---
+++ Masterpage/detalle.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Masterpage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class detalle : ContentPage
+	{
+		public detalle ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
